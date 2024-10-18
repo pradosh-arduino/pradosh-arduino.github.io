f9398458936fdadd1338e8496f17d89a036d6e62
pradosh-arduino <pradoshnathan@gmail.com>
2024-10-18 17:35:19 +0530

    Added tabs, EULA and fixed some bugs.

---
+++ tabs.js
@@ -0,0 +1,28 @@
+function open_tab(evt, subtitle) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(subtitle).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function close_all_tabs() {
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+}
