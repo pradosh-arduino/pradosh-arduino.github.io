5bdbe093203cf716a8c0cfb4d225f66db18051de
pradosh-arduino <pradoshnathan@gmail.com>
2024-10-19 15:37:05 +0530

    Added glow, fixed some bugs, added support for mobile and resizing

--- scaling.js
+++ scaling.js
@@ -1,7 +1,13 @@
 const neofetchBox = document.querySelector('.neofetch-box');
 
+function isMobileDevice() {
+  const userAgent = navigator.userAgent;
+  const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|Windows Phone|Silk|Kindle|Opera Mobi|Nokia|Symbian OS|MAUI|UC Browser|Unico Browser|Mini Browser|Dolphin|Blazer|NetFront|Teleca|Ericcson|SonyEricsson|Symbian OS|Nintendo DS|Nintendo 3DS|Nintendo Switch/;
+  return mobileRegex.test(userAgent);
+}
+
 function handleResize() {
-  if (window.innerWidth <= 568) {
+  if (window.innerWidth <= 653) {
     neofetchBox.style.display = 'none';
   } else {
     neofetchBox.style.display = 'block';
@@ -11,3 +17,9 @@ function handleResize() {
 window.addEventListener('resize', handleResize);
 
 handleResize();
+
+if (isMobileDevice()) {
+  neofetchBox.style.display = 'none';
+} else {
+  neofetchBox.style.display = 'block';
+}
