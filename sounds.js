60a7273045959d88af97b21dca73f580d4d0e144
pradosh-arduino <pradoshnathan@gmail.com>
2024-10-24 20:56:01 +0530

    Improved website and added more sounds.

--- sounds.js
+++ sounds.js
@@ -1,19 +1,29 @@
 var allButtons = document.querySelectorAll('.current-button');
+var tablinks = document.querySelectorAll(".tablinks");
 var keys = document.getElementById('keys');
 
-console.log(allButtons);
+console.log(tablinks);
 
-function playAllAudio() {
+function playKeysAudio() {
   const audioElements = document.querySelectorAll('#keys');
   audioElements.forEach(audio => audio.play());
 }
 
-function stopAllAudio() {
+function playPositiveAudio() {
+  const audioElements = document.querySelectorAll('#ui-positive');
+  audioElements.forEach(audio => audio.play());
+}
+
+function stopKeysAudio() {
   keys.pause();
   keys.currentTime = 0;
 }
 
 allButtons.forEach(button => {
-  button.addEventListener('mouseover', playAllAudio, false);
-  button.addEventListener('mouseleave', stopAllAudio, false);
+  button.addEventListener('mouseover', playKeysAudio, false);
+  button.addEventListener('mouseleave', stopKeysAudio, false);
+});
+
+tablinks.forEach(button => {
+  button.addEventListener('click', playPositiveAudio, false);
 });
